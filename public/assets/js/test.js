e140234c3f19f46ce676cb7258bda67621152d74
Rafail Abdyushev <rafaelab@mail.ru>
2024-11-02 14:34:51 +0300

    js with toast

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -9,12 +9,11 @@ document.addEventListener("DOMContentLoaded", async function() {
 
 const makeRequestSelectedToApi = async () => {
     const inputList = document.getElementsByTagName("INPUT");
-//    let inputValue = document.getElementById('order-quant-value');
     let idList = Array.from(Array.from(inputList), (item) => item.dataset.pk);
     let inputValuesList = Array.from(Array.from(inputList), (item) => item.value);;
-//    Array.from(inputList).forEach();
+
     let url = '/orders/update-quantity';
-//    let dataToSend = {id: inputValue.dataset.pk, quantity: inputValue.value, _token: $('meta[name="csrf-token"]').attr('content') };
+
     let dataToSend = {id: idList, quantity: inputValuesList, _token: $('meta[name="csrf-token"]').attr('content') };
     const request = new Request(url, {
                                 method: "POST",
@@ -32,6 +31,10 @@ const makeRequestSelectedToApi = async () => {
         const data = await response.json();
         
         console.log(data);
+        Toast.fire({
+                        icon: 'success',
+                        title: 'Количество обновлено'
+                    });
     }
     catch(error) {
         console.log(error.message);
