1435e3951baf2d7a508465f2c2878ac3832da6f0
Daniil-veles <d.suvorov@skveles.ru>
2024-11-08 16:20:01 +0300

    закрывает вкладку

--- resources/views/includes/leftbar.blade.php
+++ resources/views/includes/leftbar.blade.php
@@ -38,14 +38,23 @@
                     <a class="nav-link" href="{{ route('categories') }}"><i class="ti-control-record"></i>Категории</a>
                 </li>
                 @endcan
+                @can('view', \App\Models\Product::class)
                 <li class="nav-item"><a class="nav-link" href="{{ route('companies') }}"><i
                             class="ti-control-record"></i>Компании</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('divisions') }}"><i
-                            class="ti-control-record"></i>Подразделения</a></li>
-
-                <li class="nav-item"><a class="nav-link" href="{{ route('groups.divisions') }}"><i
-                            class="ti-control-record"></i>Группы подразделений</a></li>
+                @endcan
+                @can('view', \App\Models\Product::class)
+                <li class="nav-item">
+                        <a class="nav-link" href="{{ route('divisions') }}"><i
+                                class="ti-control-record"></i>Подразделения</a>
+                </li>
+                @endcan
+                @can('view', \App\Models\Product::class)
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('groups.divisions') }}"><i
+                            class="ti-control-record"></i>Группы подразделений</a>
+                        </li>
+                @endcan
             </ul>
         </li>
 
