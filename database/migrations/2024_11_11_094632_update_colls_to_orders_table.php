6ee1729093c0c247639a3aadfe62d9879a943487
Daniil-veles <d.suvorov@skveles.ru>
2024-11-11 13:58:48 +0300

    удаляет файл миграции

--- database/migrations/2024_11_11_094632_update_colls_to_orders_table.php
+++ database/migrations/2024_11_11_094632_update_colls_to_orders_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,13 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('permissions', function (Blueprint $table) {
-            $table->id()->from(1001);
-            $table->timestamps();
-            $table->string('name')->nullable();
-            $table->string('action');
-            $table->string('model');
-        });
+        DB::statement('');
     }
 
     /**
@@ -25,6 +20,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('permissions');
+        DB::statement('');
     }
 };
