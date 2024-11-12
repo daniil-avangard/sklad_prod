d618be4564ec541a9b666108a0dc3694fbb4de65
Daniil-veles <d.suvorov@skveles.ru>
2024-11-12 15:27:29 +0300

    добавляет флаг is_admin

--- app/Console/Commands/Users/CreateUserCommand.php
+++ app/Console/Commands/Users/CreateUserCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\Users;
 
+use App\Models\Role;
 use Illuminate\Console\Command;
 use App\Models\User;
 
@@ -13,16 +14,34 @@ class CreateUserCommand extends Command
 
     public function handle()
     {
-        $user = User::create([
-            'surname' => $this->ask('Ваша фамилия'),
-            'first_name' => $this->ask('Ваше имя'),
-            'email' => $this->ask('Ваш email'),
-            'password' => $this->ask('Ваш пароль'),
-        ]);
+        // Запрашиваем данные пользователя с валидацией
+        $surname = $this->ask('Ваша фамилия');
+        $firstName = $this->ask('Ваше имя');
+        $email = $this->ask('Ваш email');
+        $password = $this->ask('Ваш пароль'); // Используем secret для пароля
 
-        $this->warn("ID {$user->id}");
+        // Проверяем, существует ли пользователь с таким email
+        $user = User::firstOrCreate(
+            ['email' => $email],
+            ['surname' => $surname, 'first_name' => $firstName, 'password' => $password, 'is_admin' => true]
+        );
 
-        $this->info('Пользователь создан');
+        $this->warn("ID пользователя: {$user->id}");
+        $this->info('Пользователь создан.');
+
+        // Получаем роли в виде массива для выбора
+        $roleOptions = Role::all()->pluck('name', 'id')->toArray();
+
+        // Отображаем выбор пользователю с индексами
+        $selectedRole = $this->choice('Выберите роль для пользователя', $roleOptions);
+
+        // Находим ID выбранной роли по индексу
+        $selectedRoleId = array_search($selectedRole, $roleOptions);
+
+        // Присваиваем выбранную роль пользователю
+        $user->roles()->attach($selectedRoleId);
+
+        $this->info('Роль успешно назначена пользователю.');
 
         return Command::SUCCESS;
     }
