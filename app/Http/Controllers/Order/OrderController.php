d40dd05809080f745bf84e5630fd1ccc4947dc9a
Rafail Abdyushev <rafaelab@mail.ru>
2025-03-06 14:29:23 +0300

    map wherein

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -125,21 +125,29 @@ class OrderController extends Controller
     {
         $role = Auth::user()->rolesId()->pluck('id')->toArray();
         $divisionGroupsID = Auth::user()->divisionGroups()->pluck('id');
+        $divisionGroupsID1 = Auth::user()->divisionGroups()->pluck('id');
         //dd($divisionGroupsID[0]);
 //        $currentRole = ($role[0] == 1004) ? StatusEnum::PROCESSING->value : StatusEnum::NEW->value;
         $currentRole = (in_array(1004, $role)) ? StatusEnum::PROCESSING->value : StatusEnum::NEW->value;
         $allDivisionsNames = Division::all()->map(function ($division) {
             return array('name'=>$division->name, 'sort'=>$division->sort_for_excel);
         })->toArray();
-        //$groupDivisionsNames = Division::all()->get()->divisionGroups()->whereIn('division_group_id', $divisionGroupsID);
         
-        $groupDivisionsNames = Division::all()->map(function ($division) use ($divisionGroupsID) {
-            if (in_array($divisionGroupsID[0], $division->divisionGroups()->pluck('id')->toArray())) {
-                return array('name'=>$division->name, 'sort'=>$division->sort_for_excel);
-            }
+        
+//        $groupDivisionsNames = Division::all()->map(function ($division) use ($divisionGroupsID) {
+//            if (in_array($divisionGroupsID[0], $division->divisionGroups()->pluck('id')->toArray())) {
+//                return array('name'=>$division->name, 'sort'=>$division->sort_for_excel);
+//            }
+//        })->toArray();
+        
+        $groupDivisionsNames1 = Division::whereIn('id', function ($query) use ($divisionGroupsID1) {
+            $query->select('division_id')->from('division_division_group')->whereIn('division_group_id', $divisionGroupsID1);
+        })->get();
+        $groupDivisionsNames1 = $groupDivisionsNames1->map(function ($division) {
+            return array('name'=>$division->name, 'sort'=>$division->sort_for_excel);
         })->toArray();
-//        $groupDivisionsNames1 = Division::all()->toArray();
-//        dd(array_filter($groupDivisionsNames));
+        
+        $allDivisionsNames =  (in_array(1001, $role)) ? $groupDivisionsNames1 : $allDivisionsNames;
         
         $divisionStateOrders = array();
         $divisionStateOrdersNew = array();
