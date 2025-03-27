edd48412fe9b1c36ac6051ba6c8f71cff90f0f6b
Rafail Abdyushev <rafaelab@mail.ru>
2025-03-27 10:01:36 +0300

    product

---
+++ public/assets/pages/product/add-product-to-basket.js
@@ -0,0 +1,51 @@
+const addProductToBasketForms = document.querySelectorAll('.add-product-to-basket-form');
+let butonRedirectToBasket = document.getElementById('redirect-to-basket');
+let buttonsForm = document.querySelectorAll('button[type=submit]');
+
+butonRedirectToBasket.onclick = () => {
+    const url = new URL(window.location.origin);
+    url.pathname = '/basket/';
+    window.open(url, "_self");
+}
+
+addProductToBasketForms.forEach((basketForm, ind) => {
+    basketForm.addEventListener('submit', (evt) => {
+        buttonsForm[ind].disabled = true;
+        evt.preventDefault();
+
+        const data = new FormData(basketForm);
+        const productId = basketForm.dataset.productId;
+
+        const url = basketForm.action;
+
+        fetch(url, {
+            method: "POST",
+            body: data,
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Ошибка добавления товара в корзину');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log('data = ', data);
+                buttonsForm[ind].innerHTML = data.quontity + " добавлено в корзину";
+                buttonsForm[ind].classList.add("basket-button-change");
+//                let button = basketForm.getElementsByTagName("BUTTON")[0];
+//                console.log('button = ', buttonsForm[ind]);
+                Toast.fire({
+                    icon: 'success',
+                    title: data.success
+                });
+                buttonsForm[ind].disabled = false;
+                basketForm.reset();
+            })
+            .catch((error) => {
+                Toast.fire({
+                    icon: 'error',
+                    title: error.message
+                });
+            });
+    });
+});
