58181b7fddbe653c241ebf7190782af976562b62
Daniil-veles <d.suvorov@skveles.ru>
2025-04-18 16:17:36 +0300

    обнова

--- app/Console/Commands/CreatePermissionsCommand.php
+++ app/Console/Commands/CreatePermissionsCommand.php
@@ -24,7 +24,7 @@ class CreatePermissionsCommand extends Command
 
         // Создание ролей с правами
         $this->createRoleWithPermissions('Супер админ', true, range(1001, 1061));
-        $this->createRoleWithPermissions('Управляющий подразделения', false, [1016, 1027]);
+        $this->createRoleWithPermissions('Управляющий подразделения', false, [1016, 1024, 1027]);
         $this->createRoleWithPermissions('Куратор', false, [1016, 1025, 1026, 1028, 1030, 1031, 1042]);
         $this->createRoleWithPermissions('Начальник кураторов', false, [1003, 1006, 1010, 1011, 1015, 1016, 1020, 1024, 1025, 1026, 1028, 1029, 1030, 1031, 1032, 1033, 1035, 1036, 1042, 1047, 1052, 1057]);
         $this->createRoleWithPermissions('Складовщик', false, [1005, 1006, 1007, 1008, 1009, 1011, 1012, 1013, 1014, 1016, 1020, 1034, 1035, 1036, 1037, 1038, 1039]);
