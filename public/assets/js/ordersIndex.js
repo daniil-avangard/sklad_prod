076fa801ddc75435dfafd86039a3948e4ebde362
Rafail Abdyushev <rafaelab@mail.ru>
2025-04-29 13:17:05 +0300

    js for filters

--- public/assets/js/ordersIndex.js
+++ public/assets/js/ordersIndex.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let popUps = document.querySelectorAll('.order-popup-parent');
     let popUpsChilds = document.querySelectorAll('.order-popup-child');
     let selectDivision = document.getElementById('divisiones-names');
+    let selectOrderStatus = document.getElementById('status-of-orders');
 
     Array.from(popUps).forEach((el, index) => {
         const listener = () => {
@@ -30,11 +31,31 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     
     selectDivision.onchange = () => {
+        selectOrderStatus.value = "0";
         let tableTrArray = Array.from(document.getElementById('orders-table').rows);
         tableTrArray.forEach((el, index) => {
             let cell = el.cells[0].getElementsByTagName("A")[0];
             if (cell) {
                 if (cell.innerHTML.trim() != selectDivision.value && selectDivision.value != "0") {
+                    el.classList.add('row-hidden');
+                    console.log(cell.innerHTML);
+                } else {
+                    el.classList.remove('row-hidden');
+                }
+                
+            }
+            
+        });
+    }
+    
+    selectOrderStatus.onchange = () => {
+        selectDivision.value = "0";
+        let tableTrArray = Array.from(document.getElementById('orders-table').rows);
+        tableTrArray.forEach((el, index) => {
+            let cell = el.cells[3].getElementsByTagName("SPAN")[0];
+            if (cell) {
+                let text = selectOrderStatus.options[selectOrderStatus.selectedIndex].text;
+                if (cell.innerHTML.trim() != text && selectOrderStatus.value != "0") {
                     el.classList.add('row-hidden');
                     console.log(cell.innerHTML);
                 } else {
